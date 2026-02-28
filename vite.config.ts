0e2452fb52a0489cdea509211fb14aed553a4950
wbo_dbaselga <wbo_dbaselga@ext.laligatech.com>
2026-02-28 20:12:04 +0100

    Cambiar la configuración de vite para que una vez compilado config.base sea /oscars-2026/

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig, type UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ command }) => {
+  const config: UserConfig = {
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+      tailwindcss(),
+    ],
+  };
+
+  if (command !== "serve") {
+    config.base = "/oscars-2026/";
+  }
+
+  return config;
+});
