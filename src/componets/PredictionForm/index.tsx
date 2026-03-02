1db7329c4f1ac999d7f21f765d9f57645a91576c
wbo_dbaselga <wbo_dbaselga@ext.laligatech.com>
2026-03-02 10:49:32 +0100

    refactor: mover PredictionProvider a PredictionForm
    Reducir el alcance del contexto moviendo el PredictionProvider desde main.tsx al componente PredictionForm, ya que solo se utiliza dentro de este componente y sus hijos.

--- src/componets/PredictionForm/index.tsx
+++ src/componets/PredictionForm/index.tsx
@@ -1,11 +1,12 @@
+import { PredictionProvider } from "../../contexts/PredictionContext";
 import { OpenButton } from "./OpenButton";
 import { PredictionModal } from "./PredictionModal";
 
 export const PredictionForm = () => {
   return (
-    <>
+    <PredictionProvider>
       <OpenButton />
       <PredictionModal />
-    </>
+    </PredictionProvider>
   );
 };
